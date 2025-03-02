3a152a930a5d59dfdb4d72b1c5736b7d2534b8db
Ophelia Yin Hei Sin <oysin@vclvm176-62.vcl.ncsu.edu>
2025-03-01 21:48:10 -0500

    wrote skeleton logic for lock acquisition in comments

--- h/lock.h
+++ h/lock.h
@@ -1,9 +1,35 @@
 #ifndef _LOCK_H_
 #define _LOCK_H_
 
-#define DELETED
-#define READ
-#define WRITE
-#define NLOCKS  50 /* Number of locks allowed */
+#define	LFREE	'\01'		/* this lock is free		*/
+#define	LREAD	'\02'		/* this lock is read access		*/
+#define LWRITE   '\03'	    /* this lock has write access		*/
+#define LDELETED '\04'		/* this lock is deleted    */
+
+#ifndef	NLOCKS
+#define NLOCKS  50 /* number of locks allowed */
+#endif
+
+struct lentry { /* lock table entry*/
+    char lstate; /* the state LFREE, LREAD, LWRITE, or LDELETED */
+    int lcnt;   /* the count */
+    int version; /* version of lock – increments every time a lock is recreated after being deleted */
+    int lprio; /* max priority among all processes waiting in the lock's wait queue */
+
+    /* linked list of the process ids of the processes currently holding the lock */
+    
+    int rqhead; /* q index of head of readers linked list */
+    int rqtail; /* q index of tail of readers linked list */
+
+    int wqhead; /* q index of head of writers linked list */
+    int wqtail; /* q index of tail of writers linked list */
+}
+
+extern struct lentry locktab[];
+extern int nextlock;
+
+#define	isbadlock(l)	(l<0 || l>=NLOCKS)
+
+void linit();
 
 #endif
