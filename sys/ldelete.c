cc2e7069df950dcc358d27032eb3620ad938497a
Ophelia Yin Hei Sin <oysin@vclvm178-105.vcl.ncsu.edu>
2025-03-02 19:05:32 -0500

    wrote some code to ldelete; haven't fully fixed it

--- sys/ldelete.c
+++ sys/ldelete.c
@@ -1,1 +1,23 @@
-/*Destroy a lock: int ldelete (int lockdescriptor) – Deletes the lock identified by the descriptor lockdescriptor. (see “Lock Deletion” below)*/
+/*Destroy a lock: int ldelete (int lockdescriptor) – 
+Deletes the lock identified by the descriptor lockdescriptor. 
+(see “Lock Deletion” below)*/
+
+#include <conf.h>
+#include <kernel.h>
+#include <lock.h>
+
+int ldelete(int lockdescriptor) {
+    STATWORD ps; 
+
+    disable(ps);
+    struct lentry *lptr;
+
+    lptr->lstate = LDELETED; // delete the lock 
+
+    // for each process that's waiting on the lock that's being deleted, it should receive DELETED after waking up
+    int i; 
+    int curr_process = lptr->lqhead;  
+    while ()
+    restore(ps);
+    return(OK);
+}
